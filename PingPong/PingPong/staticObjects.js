40c19dc709e42aab48cfb9900d1bb427dbe56223
hristofornikolov <hristofor.nikolov@yahoo.co.uk>
2014-06-08 15:23:37 +0300

    Changes to the static objects

--- PingPong/PingPong/staticObjects.js
+++ PingPong/PingPong/staticObjects.js
@@ -5,10 +5,22 @@
 
     ctx.beginPath();
     ctx.strokeStyle = "#fff";
+    ctx.fillStyle = "#fff";
+
+    //ctx.strokeRect(10, 50, 600, 5);
+    ctx.fillRect(10, 50, 600, 5);
+
+
+
+
+
+    ctx.fillRect(10, 390, 600, 5);
+
+
     ctx.lineWidth = 5;
 
     ctx.moveTo(10, 10);
-    ctx.lineTo(10, 300);
+    ctx.lineTo(200, 100);
     ctx.stroke();
 
 
