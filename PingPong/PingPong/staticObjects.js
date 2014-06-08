fb5b4685049ff82fb3e0dee8d1bdc2a82cf9a93f
hristofornikolov <hristofor.nikolov@yahoo.co.uk>
2014-06-08 15:13:45 +0300

    Starting the Static Objects Layer

---
+++ PingPong/PingPong/staticObjects.js
@@ -0,0 +1,18 @@
+﻿window.onload = function() {
+    
+    var theCanvas = document.getElementById("static-canvas");
+    var ctx = theCanvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.strokeStyle = "#fff";
+    ctx.lineWidth = 5;
+
+    ctx.moveTo(10, 10);
+    ctx.lineTo(10, 300);
+    ctx.stroke();
+
+
+    
+
+
+}
